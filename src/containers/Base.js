179a8e5c3009f2879b66b06ac1d58c620391764a
Andrew <bg43179@gmail.com>
2020-09-24 20:10:22 -0700

    update base layout to be more responsive

--- src/containers/Base.js
+++ src/containers/Base.js
@@ -14,15 +14,15 @@ const Base = ({ title, children } : Props) => {
   return (
     <Layout title={title}>
       <Row>
-        <Col md={3} xs={12}>
+        <Col xl={4} lg={4} md={3} sm={12} xs={12}>
           <Sidebar />
         </Col>
-        <Col md={8}>
+        <Col xl={6} lg={6} md={8}>
           <Page>
             {children}
           </Page>
         </Col>
-        <Col md={2} />
+        <Col xl={2} lg={2} md={1}/>
       </Row>
     </Layout>
   );
