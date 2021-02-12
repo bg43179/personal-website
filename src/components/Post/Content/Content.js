7fbf11cc42911796b2f6b97767e1b31e90926eca
Andrew <bg43179@gmail.com>
2021-02-11 18:28:00 -0800

    make content responsive

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Row, Col } from 'react-bootstrap';
 import { FaChevronCircleLeft } from 'react-icons/fa';
 import styles from './Content.module.scss';
 
@@ -9,13 +10,19 @@ type Props = {
 };
 
 const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <Link className="d-inline-block mt-4 btn btn-outline-dark rounded-pill" to="/page">
-      <FaChevronCircleLeft className="mx-2"/><span>Back</span>
-    </Link>
-    <h1 className="d-flex my-4 justify-content-center">{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
-  </div>
+    <Row>
+      <Col xl={2} lg={1} md={1} sm={1} xs={1}/>
+      <Col>
+        <div className="px-5 pt-5">
+          <Link className="d-inline-block mt-4 btn btn-outline-dark rounded-pill" to="/page">
+            <FaChevronCircleLeft className="mx-2"/><span>Back</span>
+          </Link>
+          <h1 className="d-flex my-4 justify-content-center">{title}</h1>
+          <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+        </div>
+      </Col>
+      <Col xl={2} lg={1} md={1} sm={1} xs={1}/>
+    </Row>
 );
 
 export default Content;
